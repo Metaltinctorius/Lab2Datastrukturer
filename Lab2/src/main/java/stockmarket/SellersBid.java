20a76ff4ab14f98da1567d00eb8716838c1c3310
nikand <niklas.andersson91@hotmail.com>
2023-04-23 02:40:35 +0200

    First commit, test if building project works

---
+++ Lab2/src/main/java/stockmarket/SellersBid.java
@@ -0,0 +1,9 @@
+package stockmarket;
+import java.util.Comparator;
+
+public class SellersBid implements Comparator<Bid>  {
+
+    public int compare(Bid o1, Bid o2){
+        return -Integer.compare(o1.getBid(), o2.getBid());
+    }
+}
